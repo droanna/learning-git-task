e1132b5cc5d80192cc57fa6498038caec05c5ca9
Anna Drożdż <annadrozdz33@gmail.com>
2025-06-23 00:05:07 +0200

    items count

--- task.py
+++ task.py
@@ -3,6 +3,9 @@ shopping_list = {
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
 
+count = 0
 for key, values in shopping_list.items():
     new_values = [v.capitalize() for v in values]
     print(f"Idę do {key.capitalize()} i kupuję tam: {new_values}")
+    count += len(values)
+print(f"W sumie kupuję {count} produktów.")
